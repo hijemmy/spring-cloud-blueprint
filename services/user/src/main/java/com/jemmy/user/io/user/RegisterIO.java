7a6bc5d1ee29804046d40f8f5d747b7c31671f60
Jemmy <caiqingh2006@126.com>
2018-11-09 17:07:24 +0800

    将微服务间调用Api抽离成单独项目

--- services/user/src/main/java/com/jemmy/user/io/user/RegisterIO.java
+++ services/user/src/main/java/com/jemmy/user/io/user/RegisterIO.java
@@ -1,10 +1,10 @@
-package com.jemmy.common.io.user.user;
+package com.jemmy.user.io.user;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
-public class RegisterIO{
+public class RegisterIO {
     @NotEmpty
     private String username;
     @NotEmpty
