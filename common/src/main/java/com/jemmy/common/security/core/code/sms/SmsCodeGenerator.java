59548b5207c139ce3da943a341e038668c19bd1a
Jemmy <caiqingh2006@126.com>
2018-11-10 12:54:01 +0800

    添加用户中心服务间调用api及相关的安全模块

---
+++ common/src/main/java/com/jemmy/common/security/core/code/sms/SmsCodeGenerator.java
@@ -0,0 +1,34 @@
+package com.jemmy.common.security.core.code.sms;
+
+import com.jemmy.common.security.core.code.ValidateCode;
+import com.jemmy.common.security.core.code.ValidateCodeGenerator;
+import com.jemmy.common.security.core.properties.SecurityProperties;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletWebRequest;
+
+/**
+ * 短信验证码生成器
+ *
+ * @author paascloud.net @gmail.com
+ */
+@Component("smsValidateCodeGenerator")
+public class SmsCodeGenerator implements ValidateCodeGenerator {
+
+	@Autowired
+	private SecurityProperties securityProperties;
+
+	/**
+	 * Generate validate code.
+	 *
+	 * @param request the request
+	 *
+	 * @return the validate code
+	 */
+	@Override
+	public ValidateCode generate(ServletWebRequest request) {
+		String code = RandomStringUtils.randomNumeric(securityProperties.getCode().getSms().getLength());
+		return new ValidateCode(code, securityProperties.getCode().getSms().getExpireIn());
+	}
+}
---
+++ common/src/main/java/com/jemmy/common/security/core/code/sms/SmsCodeSender.java
@@ -0,0 +1,19 @@
+package com.jemmy.common.security.core.code.sms;
+
+/**
+ * The interface Sms code sender.
+ *
+ * @author paascloud.net@gmail.com
+ */
+public interface SmsCodeSender {
+
+	/**
+	 * Send.
+	 *
+	 * @param mobile the mobile
+	 * @param code   the code
+	 * @param ip     the ip
+	 */
+	void send(String mobile, String code, String ip);
+
+}
