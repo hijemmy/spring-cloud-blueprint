344f053998c295bfb20f77c4a1c14099295630c1
Jemmy <caiqingh2006@126.com>
2018-11-16 17:13:24 +0800

    1. 优化代码 2. 删除一部分无用代码

--- services/user/src/main/java/com/jemmy/services/user/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jemmy.services.user.mapper;
-
-import com.jemmy.common.core.mybatis.RootMapper;
-import com.jemmy.services.user.model.User;
-
-public interface UserMapper extends RootMapper<User> {
-}
