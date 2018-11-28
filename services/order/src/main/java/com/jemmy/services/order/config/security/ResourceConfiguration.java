1fd412529811bc485be29e839edb15eed791748f
Jemmy <caiqingh2006@126.com>
2018-11-28 10:10:18 +0800

    1. 修改user服务日志格式 2. 修改order及product的资源服务器配置

--- services/order/src/main/java/com/jemmy/services/order/config/security/ResourceConfiguration.java
+++ services/order/src/main/java/com/jemmy/services/order/config/security/ResourceConfiguration.java
@@ -1,25 +1,13 @@
 package com.jemmy.services.order.config.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jemmy.common.security.Pbkdf2PasswordEncoder;
-import com.jemmy.common.security.SelfAccessDeniedHandler;
-import com.jemmy.common.security.SelfAuthenticationEntryPoint;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableResourceServer
@@ -27,59 +15,16 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @RefreshScope
 public class ResourceConfiguration extends ResourceServerConfigurerAdapter{
 
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Value("${com.jemmyweb.order.urls.permitall}")
-    private String[] urls;
-
-    @Value("${security.oauth2.resource.jwt.key-value}")
-    private String signKey;
-
-    @Autowired
-    OAuth2ClientContext oAuth2ClientContext;
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        http.csrf().disable()
-            .authorizeRequests()
-                .antMatchers(urls).permitAll()
+        http
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
                 .and()
                 .authorizeRequests().antMatchers("/pay/alipayCallback", "/druid/**", "/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/api/applications").permitAll()
                 .anyRequest().authenticated();
-    }
-
-    //使用JWT作为token
-    @Bean
-    public TokenStore jwtTokenStore() {
-        return new JwtTokenStore(jwtAccessTokenConverter());
-    }
-
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        //设置签名密钥
-        jwtAccessTokenConverter.setSigningKey(signKey);
-        return jwtAccessTokenConverter;
-    }
-
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(jwtTokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
-    }
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.accessDeniedHandler(new SelfAccessDeniedHandler(objectMapper)).authenticationEntryPoint(new SelfAuthenticationEntryPoint(objectMapper));
-    }
-
-    @Bean
-    public Pbkdf2PasswordEncoder passwordEncoder() {
-        return new Pbkdf2PasswordEncoder(512,1000,16);
     }
 }
