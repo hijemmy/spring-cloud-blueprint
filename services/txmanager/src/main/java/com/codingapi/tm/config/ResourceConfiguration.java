ec528d3f8f1e330d93d54fbd708182899ec83d95
Jemmy <caiqingh2006@126.com>
2018-12-18 12:47:38 +0800

    1.  添加基于lcn分布式事务支持

---
+++ services/txmanager/src/main/java/com/codingapi/tm/config/ResourceConfiguration.java
@@ -0,0 +1,38 @@
+package com.codingapi.tm.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.juneng.common.security.server.SelfAccessDeniedHandler;
+import com.juneng.common.security.server.SelfAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+/**
+ * @author Jemmy
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceConfiguration extends ResourceServerConfigurerAdapter{
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new SelfAuthenticationEntryPoint(objectMapper))
+                .and()
+                .authorizeRequests().antMatchers("/pay/alipayCallback","/tx/**", "/druid/**", "/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/api/applications").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.accessDeniedHandler(new SelfAccessDeniedHandler(objectMapper)).authenticationEntryPoint(new SelfAuthenticationEntryPoint(objectMapper));
+    }
+}
